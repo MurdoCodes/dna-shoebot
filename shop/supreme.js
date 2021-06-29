a00ce34293fadf187ecbf147c84b6bf0d596585c
Lidel Kim Daddie <murdoc21daddie@gmail.com>
2021-06-29 10:18:15 +0800

    change directory /usr/lib/chromium/chromium

--- shop/supreme.js
+++ shop/supreme.js
@@ -64,6 +64,7 @@ async function checkout(userBotData, res){
     ]
     const options = {  
         headless: false,
+        executablePath: '/usr/lib/chromium/chromium',
         slowMo: 35,
         ignoreHTTPSErrors: true,
         ignoreDefaultArgs: ["--enable-automation"],
